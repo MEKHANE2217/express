c4a6649d62cf5440013f32d0709956a0bfdfc013
MEKHANE2217 <watanabe.keigo7@gmail.com>
2023-12-03 17:58:04 +0900

    Append QR var2

--- routes/QR.js
+++ routes/QR.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+
 router.get('/', async (req, res) => {
-request('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example', function (error, response,
-body) {
-if (!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-res.json(data);
-}
+    const url = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example';
+
+    request({ url, encoding: null }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.setHeader('Content-Type', 'image/png');
+            res.send(body);
+        } else {
+            res.status(500).send('Error generating QR code');
+        }
+    });
 });
-})
+
 module.exports = router;
